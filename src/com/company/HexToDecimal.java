eec7b94d10926963fd4cf649ef272be250da1c00
FabianV92 <71566212+FabianV92@users.noreply.github.com>
2021-01-25 07:25:13 +0100

    New Challenge

--- src/com/company/HexToDecimal.java
+++ src/com/company/HexToDecimal.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class HexToDecimal {
 
-        public static int hexToDec(final String hexString) {
+    public static int hexToDec(final String hexString) {
             return Integer.parseInt(hexString,16);
         }
 
---
+++ src/com/company/SumOfOddNumbsOfAnTriangle.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+//Given the triangle of consecutive odd numbers:
+           /* 1
+           3     5
+        7     9    11
+     13    15    17    19
+  21    23    25    27    29
+...*/
+//Calculate the row sums of this triangle from the row index (starting at index 1)
+public class SumOfOddNumbsOfAnTriangle {
+    public static int rowSumOddNumbers(int n) {
+    int rowIncrNumb = 1;
+    int oddNumb  = 1;
+    int sumOfOddNumbr = 0;
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 0; j < rowIncrNumb ; j++) {
+                if (i == n) {
+                sumOfOddNumbr += oddNumb;
+                }
+                oddNumb+=2;
+            }
+            rowIncrNumb++;
+        }
+    return  sumOfOddNumbr;
+    }
+    public static void main(String[] args) {
+        System.out.println(rowSumOddNumbers(42));
+    }
+}
