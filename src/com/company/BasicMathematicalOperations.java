72d6edbb49d70438df2ae01f72354aa8cb36109e
FabianV92 <71566212+FabianV92@users.noreply.github.com>
2021-02-18 17:01:21 +0100

    Added new Katas

---
+++ src/com/company/BasicMathematicalOperations.java
@@ -0,0 +1,36 @@
+package com.company;
+/*
+Kyu (8)
+Basic Mathematical Operations
+
+The function should return result of numbers after applying the chosen operation.
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+
+        Examples
+
+        basicOp('+', 4, 7)         // Output: 11
+        basicOp('-', 15, 18)       // Output: -3
+        basicOp('*', 5, 5)         // Output: 25
+        basicOp('/', 49, 7)        // Output: 7
+*/
+
+public class BasicMathematicalOperations {
+
+    public static Integer basicMath(String op, int v1, int v2) {
+        switch (op) {
+            case "+":
+                return v1 + v2;
+            case "-":
+                return v1 - v2;
+            case "*":
+                return v1 * v2;
+        }
+        return v1 / v2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(basicMath("/", 49, 7));
+    }
+}
---
+++ src/com/company/SupermarktQue.java
@@ -0,0 +1,51 @@
+package com.company;
+/*
+Kyu (6)
+The Supermarket Queue
+
+There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total
+time required for all the customers to check out!
+
+        input
+customers: an array of positive integers representing the queue. Each integer represents a customer, and its
+value is the amount of time they require to check out.
+n: a positive integer, the number of checkout tills.
+
+        Examples
+
+        queueTime([5,3,4], 1)
+// should return 12
+// because when there is 1 till, the total time is just the sum of the times
+
+        queueTime([10,2,3,3], 2)
+// should return 10
+// because here n=2 and the 2nd, 3rd, and 4th people in the
+// queue finish before the 1st person has finished.
+
+        queueTime([2,3,10], 2)
+// should return 12
+
+*/
+
+import java.util.Arrays;
+
+public class SupermarktQue {
+
+    public static int solveSuperMarketQueue(int[] customers, int n) {
+        int[] cashier = new int[n];
+
+        // Looping via  for loop through each customer and give each cashier customers
+        // And sort the cashier array via the arrays.sort method
+        // which means cashier with the index 0 will always be the cashier with the lessed amount of costumers
+        for (int i = 0; i < customers.length;i++) {
+                cashier[0] += customers[i];
+                Arrays.sort(cashier);
+        }
+        // Returning the cashier with the most customers
+        return cashier[cashier.length-1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solveSuperMarketQueue(new int[]{5,3,4}, 1));// => should return 12
+    }
+}
