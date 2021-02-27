ee937d37f0ce7572ab564ef35d289e96a4a97350
FabianV92 <71566212+FabianV92@users.noreply.github.com>
2021-02-27 16:30:48 +0100

    Added Javadoc comments

--- src/com/FabianCodeWars/kyu8Exercises/BasicMathematicalOperations.java
+++ src/com/FabianCodeWars/kyu8Exercises/BasicMathematicalOperations.java
@@ -18,6 +18,15 @@ The function should take three arguments - operation(string/char), value1(number
 
 public class BasicMathematicalOperations {
 
+    /**
+     * The method takes 3 arguments. The first argument uses a switch case to choose which
+     * operation needs to be done. Multiplies/ subtract or adds v1 input and v2 input.
+     *
+     * @param op The input which is one of the three operators"+,-,*".
+     * @param v1 Is a numeric int digit
+     * @param v2 Is a numeric int digit
+     * @return Returns the result of input v1 and input v2 depending which operator input op is used.
+     */
     public static Integer basicMath(String op, int v1, int v2) {
         switch (op) {
             case "+":
--- src/com/FabianCodeWars/kyu8Exercises/HexToDecimal.java
+++ src/com/FabianCodeWars/kyu8Exercises/HexToDecimal.java
@@ -1,14 +1,21 @@
 package com.FabianCodeWars.kyu8Exercises;
+
 //Kyu(8) if I remember right
 public class HexToDecimal {
 
+    /**
+     * Method converts a hexa decimal String to a decimal number with the
+     * Integer.parseInt hexString method and the radix of 16.
+     *
+     * @param hexString Is a hexa decimal String.
+     * @return the converted decimal number as an int.
+     */
     public static int hexToDec(final String hexString) {
-            return Integer.parseInt(hexString,16);
-        }
+        return Integer.parseInt(hexString, 16);
+    }
 
-        public static void main(String[] args)
-        {
-            System.out.println(hexToDec("F"));
-        }
+    public static void main(String[] args) {
+        System.out.println(hexToDec("F"));
+    }
 }
 
