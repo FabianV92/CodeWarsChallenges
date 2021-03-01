4addf867b5757791774359e2d1b62b851cf97dc0
FabianV92 <71566212+FabianV92@users.noreply.github.com>
2021-03-01 21:45:19 +0100

    Added Javadoc comment

--- src/com/FabianCodeWars/kyu8Exercises/SumOfOddNumbsOfAnTriangle.java
+++ src/com/FabianCodeWars/kyu8Exercises/SumOfOddNumbsOfAnTriangle.java
@@ -1,4 +1,5 @@
 package com.FabianCodeWars.kyu8Exercises;
+
 // Kyu (7) or (8)
 // Given the triangle of consecutive odd numbers:
            /* 1
@@ -10,21 +11,29 @@ package com.FabianCodeWars.kyu8Exercises;
 // Calculate the row sums of this triangle from the row index (starting at index 1)
 public class SumOfOddNumbsOfAnTriangle {
 
+    /**
+     * This method takes an int and loops through two for loops, the first is the height, the second is for the width
+     * of the 'triangle' when it reaches the end of the loop,calculates the sum of the n row of the triangle.
+     *
+     * @param n is a digital number, the primitive datatype int.
+     * @return returns the sum of the row of n from the triangle.
+     */
     public static int rowSumOddNumbers(int n) {
-    int rowIncrNumb = 1;
-    int oddNumb  = 1;
-    int sumOfOddNumb = 0;
-        for (int i = 1; i <= n ; i++) {
-            for (int j = 0; j < rowIncrNumb ; j++) {
+        int rowIncrNumb = 1;
+        int oddNumb = 1;
+        int sumOfOddNumb = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < rowIncrNumb; j++) {
                 if (i == n) {
-                sumOfOddNumb += oddNumb;
+                    sumOfOddNumb += oddNumb;
                 }
-                oddNumb+=2;
+                oddNumb += 2;
             }
             rowIncrNumb++;
         }
-    return  sumOfOddNumb;
+        return sumOfOddNumb;
     }
+
     // Could also be written like: n*n*n LOL!
     public static void main(String[] args) {
         System.out.println(rowSumOddNumbers(42));
