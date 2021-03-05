1e9a13cad2d3016e01b9b6b7bc900b2de6a2bdb8
FabianV92 <71566212+FabianV92@users.noreply.github.com>
2021-03-05 21:49:49 +0100

    Added new CodeWars solution

---
+++ src/com/FabianCodeWars/kyu7Exercises/TheOldSwitch.java
@@ -0,0 +1,36 @@
+package com.FabianCodeWars.kyu7Exercises;
+/*
+The old switcheroo
+
+Kyu(7)
+Write a function
+
+vowel2Index(String s)
+that takes in a string and replaces all the vowels [a,e,i,o,u] with their respective positions within
+that string.
+E.g:
+
+("this is my string") == "th3s 6s my str15ng"
+("Codewars is the best site in the world") == "C2d4w6rs 10s th15 b18st s23t25 27n th32 w35rld"
+*/
+
+public class TheOldSwitch {
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public static String vowel2Index(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length();i++) {
+            sb = s.substring(i, i+1).matches("[aeiou]")? sb.append(i+1) :
+            sb.append(s.substring(i,i+1));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(vowel2Index("this is my string"));
+    }
+}
